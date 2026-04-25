ede02ae0d7158c5eb8f57cb1db77b3412b3d91ef
daxida <galmaw@hotmail.com>
2026-04-25 09:54:39 +0200

    Add test_trans prototype

---
+++ test_trans.py
@@ -0,0 +1,30 @@
+from trans import repl_trans
+
+
+def mktest(raw: str, expected: str) -> None:
+    raw = raw.strip()
+    expected = expected.strip()
+    received = repl_trans(raw)
+    for idx, (exp, rec) in enumerate(
+        zip(expected.splitlines(), received.splitlines()),
+    ):
+        if exp != rec:
+            print(f"First diff at {idx=}:\n{exp=}\n{rec=}")
+            break
+    assert expected == received, received
+
+
+def test_repl_trans_base() -> None:
+    raw = """
+===={{trans}}==== 
+*{{de}}: [[Lebenslauf]]
+*{{en}}: [[history]], [[record]], [[career]]
+*{{fr}}: [[carrière]]
+"""
+    expected = """
+===={{trans}}==== 
+*{{T|de}}: {{t|de|Lebenslauf}}
+*{{T|en}}: {{t|en|history}}, {{t|en|record}}, {{t|en|career}}
+*{{T|fr}}: {{t|fr|carrière}}
+        """
+    mktest(raw, expected)
---
+++ trans.py
@@ -0,0 +1,39 @@
+import re
+
+from main import (
+    Header,
+    try_repl_with_callback,
+)
+
+
+def try_repl_trans(s: str, header: Header) -> str | None:
+    return try_repl_with_callback(s, header, try_repl_trans_section)
+
+
+def try_repl_trans_section(section: list[str], header: Header) -> list[str] | None:
+    def repl_line(line: str) -> str:
+        m = re.match(r"\*\{\{(\w+)\}\}: (.+)", line)
+        if not m:
+            return line
+        lang = m.group(1)
+        words = re.findall(r"\[\[(.+?)\]\]", m.group(2))
+        translations = ", ".join(f"{{{{t|{lang}|{w}}}}}" for w in words)
+        return f"*{{{{T|{lang}}}}}: {translations}"
+
+    return [repl_line(line) for line in section]
+
+
+def repl_trans(s: str) -> str:
+    for header in ("trans",):
+        s = try_repl_trans(s, header) or s
+    return s
+
+
+def main() -> None:
+    import test_trans
+
+    test_trans.test_repl_trans_base()
+
+
+if __name__ == "__main__":
+    main()
